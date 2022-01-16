5676ee74455f9a15b93ffcb9669442bdbd554697
Cícero Romão <romaofilho500@gmail.com>
2022-01-16 16:05:07 -0300

    Project created

---
+++ backend/src/main/java/com/devcicero/devcicero/DevciceroApplication.java
@@ -0,0 +1,13 @@
+package com.devcicero.devcicero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevciceroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevciceroApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devcicero/devcicero/DevciceroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcicero.devcicero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevciceroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
